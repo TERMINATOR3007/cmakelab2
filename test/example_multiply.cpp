2b0e6cb5f44b53e40aa2460e3322ec8b54bc7f82
TERMINATOR3007 <ivanfilyushov@gmail.com>
2026-06-02 08:55:50 +0300

    Update example_multiply.cpp

--- test/example_multiply.cpp
+++ test/example_multiply.cpp
@@ -8,15 +8,15 @@ TEST(example, multiply)
     res = multiply_numbers(2.0, 3.0);
     ASSERT_NEAR(res, 6.0, 1.0e-11);
     
-    res = multiply_numbers(5.0, 0.0);
+    res = multiply_numbers(1.0, 0.0);
     ASSERT_NEAR(res, 0.0, 1.0e-11);
     
-    res = multiply_numbers(-2.0, 4.0);
-    ASSERT_NEAR(res, -8.0, 1.0e-11);
+    res = multiply_numbers(-2.0, 3.0);
+    ASSERT_NEAR(res, -6.0, 1.0e-11);
     
-    res = multiply_numbers(-3.0, -4.0);
-    ASSERT_NEAR(res, 12.0, 1.0e-11);
+    res = multiply_numbers(-2.0, -3.0);
+    ASSERT_NEAR(res, 6.0, 1.0e-11);
     
-    res = multiply_numbers(7.0, 1.0);
-    ASSERT_NEAR(res, 7.0, 1.0e-11);
+    res = multiply_numbers(2.0, 1.0);
+    ASSERT_NEAR(res, 2.0, 1.0e-11);
 }
