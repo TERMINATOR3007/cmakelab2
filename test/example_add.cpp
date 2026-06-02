8a0f5bc8e21d14afde92cdc63581466b38f4ab0f
TERMINATOR3007 <ivanfilyushov@gmail.com>
2026-06-02 08:56:14 +0300

    Update example_add.cpp

--- test/example_add.cpp
+++ test/example_add.cpp
@@ -6,4 +6,10 @@ TEST(example, add)
     double res;
     res = add_numbers(1.0, 2.0);
     ASSERT_NEAR(res, 3.0, 1.0e-11);
+
+    res = add_numbers(1.0, -1.0);
+    ASSERT_NEAR(res, 0.0, 1.0e-11); 
+
+    res = add_numbers(1.0, -2.0);
+    ASSERT_NEAR(res, -1.0, 1.0e-11); 
 }
